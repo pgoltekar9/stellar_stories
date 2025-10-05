6c1183fd136f58965d06b2d1002e77ff0871d431
pgoltekar9 <149804273+pgoltekar9@users.noreply.github.com>
2025-10-05 11:40:42 +0530

    hjhh

--- android/app/src/main/kotlin/com/example/stellar_stories/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.stellar_stories
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/routes/app_routes.dart
+++
@@ -1,6 +0,0 @@
-class AppRoutes {
-  static const splash = '/';
-  static const home = '/home';
-  static const story = '/story';
-  static const game = '/game';
-}
